b0643789c0f5ad1a6efd4a48f9a21bb2f7b26ef0
gayeon7877 <gayeon01@gmail.com>
2024-11-06 08:00:15 +0000

    style:reformatting

--- code/LLM_aug.py
+++ code/LLM_aug.py
@@ -1,8 +1,9 @@
-import re
 import csv
-import random
+import re
+
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
 
 # 모델과 토크나이저 로드
 model_name = "LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct"
@@ -12,6 +13,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 # GPU 사용 가능 시 모델을 GPU로 이동
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
+
 
 def generate_article(title):
     prompt = f"""다음 기사 제목에 대한 내용을 작성해주세요.
@@ -28,6 +30,7 @@ def generate_article(title):
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return generated_text.split("기사 내용:")[1].strip()
 
+
 def generate_new_title(article):
     prompt = f"""다음 기사 내용을 바탕으로 새로운 제목을 생성해주세요.
     절대 기사 내용에 들어간 단어는 쓰지 마세요.:
@@ -42,33 +45,37 @@ def generate_new_title(article):
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return generated_text.split("새로운 제목:")[-1].strip()
 
+
 def save_to_csv(data, filename):
     with open(filename, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
         writer.writerow(["ID", "text", "target"])
         for i, (text, target) in enumerate(data):
-            cleaned_text = re.sub(r'^"|"$', '', text)
-            cleaned_text = re.sub(r'\s*이\s*제목은.*$', '', cleaned_text)
-            cleaned_text = ' '.join(cleaned_text.split())
+            cleaned_text = re.sub(r'^"|"$', "", text)
+            cleaned_text = re.sub(r"\s*이\s*제목은.*$", "", cleaned_text)
+            cleaned_text = " ".join(cleaned_text.split())
             writer.writerow([f"ynat-v1_train_{i:05d}", cleaned_text, target])
+
 
 def process_title(title, target):
     article = generate_article(title)
     new_title = generate_new_title(article)
     return article, new_title, target
 
+
 def read_csv(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         reader = csv.reader(file)
         next(reader)  # 헤더 스킵
         return [(row[1], row[2]) for row in reader]  # (text, target) 튜플 리스트 반환
 
+
 def process_csv(input_filename, output_filename, max_rows=None):
     data = read_csv(input_filename)
-    
+
     # 텍스트 길이에 따라 정렬
     data.sort(key=lambda x: len(x[0]), reverse=True)
-    
+
     if max_rows:
         data = data[:max_rows]
 
@@ -85,6 +92,7 @@ def process_csv(input_filename, output_filename, max_rows=None):
 
     save_to_csv(processed_data, output_filename)
     print(f"처리 완료. 결과가 '{output_filename}' 파일에 저장되었습니다.")
+
 
 # 사용 예시
 input_csv = "../data/semi-final4.csv"  # 입력 CSV 파일명
